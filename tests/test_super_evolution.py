bdefa9c83a655a8a54dd80cc6953551cafe5ed92
FreddyE1982 <frederik.egger@gmail.com>
2025-07-18 21:36:01 +0200

    Extend super evolution to nested params

--- tests/test_super_evolution.py
+++ tests/test_super_evolution.py
@@ -19,3 +19,38 @@ def test_super_evolution_records_metrics():
     brain.train(examples, epochs=1, validation_examples=examples)
     assert brain.super_evo_controller.history
 
+
+def test_super_evolution_affects_all_parameters():
+    params = minimal_params()
+    core = Core(params)
+    nb = Neuronenblitz(core)
+    import marble_brain as mb
+    mb.tqdm = std_tqdm
+    brain = Brain(core, nb, DataLoader(), super_evolution_mode=True)
+    brain.lobe_manager.genesis([0])
+    brain.core.neurons[0].attention_score = 1.0
+    brain.lobe_manager.update_attention()
+
+    brain.meta_controller.adjustment = 0.5
+    brain.neuromodulatory_system.signals["arousal"] = 1.0
+    brain.memory_system.threshold = 0.5
+
+    orig_values = (
+        brain.core.params["vram_limit_mb"],
+        brain.neuronenblitz.learning_rate,
+        brain.mutation_rate,
+        brain.meta_controller.adjustment,
+        brain.neuromodulatory_system.signals["arousal"],
+        brain.memory_system.threshold,
+    )
+
+    brain.super_evo_controller.record_metrics(0.5, 1.0)
+    brain.super_evo_controller.record_metrics(1.0, 1.1)
+
+    assert brain.core.params["vram_limit_mb"] != orig_values[0]
+    assert brain.neuronenblitz.learning_rate != orig_values[1]
+    assert brain.mutation_rate != orig_values[2]
+    assert brain.meta_controller.adjustment != orig_values[3]
+    assert brain.neuromodulatory_system.signals["arousal"] != orig_values[4]
+    assert brain.memory_system.threshold != orig_values[5]
+
