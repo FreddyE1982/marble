f54e4e6da5493bd735f70342e91f12de9a416662
FreddyE1982 <frederik.egger@gmail.com>
2025-07-24 21:04:43 +0200

    Improve Neuronenblitz stability

--- tests/test_benchmark_autograd_vs_marble.py
+++ tests/test_benchmark_autograd_vs_marble.py
@@ -1,4 +1,8 @@
+import os
+import sys
 import pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from benchmark_autograd_vs_marble import (
     generate_dataset,
