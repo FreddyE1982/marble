bb4ec86d13c1a0f5b4c786edc9336df48445e1e3
FreddyE1982 <frederik.egger@gmail.com>
2025-07-31 21:23:09 +0200

    Add borrow context manager and preallocation to MemoryPool

--- memory_pool.py
+++ memory_pool.py
@@ -3,7 +3,8 @@
 from __future__ import annotations
 
 from collections import deque
-from typing import Callable
+from typing import Callable, Iterator
+import contextlib
 
 
 class MemoryPool:
@@ -22,3 +23,23 @@ class MemoryPool:
     def release(self, obj: object) -> None:
         if len(self._free) < self.max_size:
             self._free.append(obj)
+
+    def preallocate(self, count: int) -> None:
+        """Pre-fill the pool with ``count`` new objects."""
+        for _ in range(count):
+            if len(self._free) >= self.max_size:
+                break
+            self._free.append(self.factory())
+
+    def __len__(self) -> int:  # pragma: no cover - trivial
+        """Return the number of available objects in the pool."""
+        return len(self._free)
+
+    @contextlib.contextmanager
+    def borrow(self) -> Iterator[object]:
+        """Context manager yielding an allocated object and releasing it on exit."""
+        obj = self.allocate()
+        try:
+            yield obj
+        finally:
+            self.release(obj)
