2bf62112b30077cb85d74dd1fd62f2e6d8194def
FreddyE1982 <frederik.egger@gmail.com>
2025-08-07 15:26:20 +0200

    feat: add seeking and virtual batches

--- tests/test_bit_tensor_streaming_dataset.py
+++ tests/test_bit_tensor_streaming_dataset.py
@@ -3,26 +3,55 @@ import torch
 from bit_tensor_streaming_dataset import BitTensorStreamingDataset
 
 
-def pair_stream(n=3):
-    for i in range(n):
-        yield {"input": i}, {"target": i + 1}
+class MockHFStreamingDataset:
+    """Minimal mock of a HuggingFace streaming dataset."""
+
+    def __init__(self, data):
+        self._data = list(data)
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def select(self, indices):
+        return (self._data[i] for i in indices if 0 <= i < len(self._data))
 
 
-def test_streaming_single_records():
-    ds = BitTensorStreamingDataset(pair_stream(3), batch_size=1)
-    items = list(ds)
-    assert len(items) == 3
-    for inp, tgt in items:
-        assert inp.shape[1] == 8
-        assert tgt.shape[1] == 8
-        assert isinstance(inp, torch.Tensor)
+def make_dataset(n=5):
+    return MockHFStreamingDataset([(i, i + 1) for i in range(n)])
 
 
-def test_streaming_batches():
-    ds = BitTensorStreamingDataset(pair_stream(5), batch_size=2)
-    batches = list(ds)
-    assert len(batches) == 3
-    first_inputs, first_targets = batches[0]
-    assert first_inputs.shape[0] == 2
-    last_inputs, _ = batches[-1]
-    assert last_inputs.shape[0] == 1
+def test_seek_operations():
+    ds = BitTensorStreamingDataset(make_dataset(), batch_size=1)
+    ds.seek_to(2)
+    inp, tgt = next(iter(ds))
+    assert ds.encoder.decode_tensor(inp) == 2
+    assert ds.encoder.decode_tensor(tgt) == 3
+    assert isinstance(inp, torch.Tensor)
+
+    ds.seek_backward(1)
+    inp, tgt = next(iter(ds))
+    assert ds.encoder.decode_tensor(inp) == 1
+    assert isinstance(inp, torch.Tensor)
+
+    ds.seek_forward(2)
+    inp, tgt = next(iter(ds))
+    assert ds.encoder.decode_tensor(inp) == 3
+    assert isinstance(inp, torch.Tensor)
+
+
+def test_virtual_batches():
+    ds = BitTensorStreamingDataset(make_dataset(), batch_size=1, virtual_batch_size=2)
+    batch = ds.get_virtual_batch(1, stream=False)
+    assert len(batch) == 2
+    first = ds.encoder.decode_tensor(batch[0][0])
+    second = ds.encoder.decode_tensor(batch[1][0])
+    assert (first, second) == (2, 3)
+    assert isinstance(batch[0][0], torch.Tensor)
+
+    streamed = list(ds.get_virtual_batch(0, stream=True))
+    assert ds.encoder.decode_tensor(streamed[0][0]) == 0
+    assert isinstance(streamed[0][0], torch.Tensor)
+
+    # Re-access previously requested batch
+    again = ds.get_virtual_batch(1, stream=False)
+    assert ds.encoder.decode_tensor(again[0][0]) == 2
